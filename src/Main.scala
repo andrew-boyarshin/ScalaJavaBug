2eb3d7faf7f661188697131b07186ddd51165e9f
Andrew Boyarshin <andrew.boyarshin@gmail.com>
2023-09-08 22:15:44 +0700

    Initial commit

---
+++ src/Main.scala
@@ -0,0 +1,7 @@
+package bug
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+  }
+}
---
+++ src/code/Code.java
@@ -0,0 +1,11 @@
+package bug.code;
+
+import bug.util.List;
+import bug.util.*;
+import java.util.*;
+
+public class Code {
+    public List<Long> work(List<Integer> list) {
+        return null;
+    }
+}
